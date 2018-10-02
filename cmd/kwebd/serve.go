cf0f8a0ac262e8ab83d151ef0fc28cef78933b57
Simon Eisenmann <s.eisenmann@kopano.com>
2018-10-02 17:41:10 +0200

    Add commandline options for serve command

--- cmd/kwebd/serve.go
+++ cmd/kwebd/serve.go
@@ -26,17 +26,91 @@ func commandServe() *cobra.Command {
 				os.Exit(1)
 			}
 		},
-		DisableFlagParsing: true,
 	}
+
+	serveCmd.Flags().Bool("agree", false, "Agree to the CA's Subscriber Agreement")
+	serveCmd.Flags().String("ca", "https://acme-v02.api.letsencrypt.org/directory", "URL to certificate authority's ACME server directory")
+	serveCmd.Flags().String("email", "", "ACME CA account email address")
+	serveCmd.Flags().Bool("http2", true, "Use HTTP/2")
+	serveCmd.Flags().Bool("quic", false, "Use experimental QUIC")
+	serveCmd.Flags().String("root", ".", "Path to web root")
+	serveCmd.Flags().Bool("validate", false, "Parse and validate the configuration but do not start the server")
+	serveCmd.Flags().String("revoke", "", "Hostname for which to revoke the certificate")
+	serveCmd.Flags().String("host", "0.0.0.0", "Hostname to serve")
+	serveCmd.Flags().String("http-port", "80", "Port to use for HTTP")
+	serveCmd.Flags().String("https-port", "443", "Port to use for HTTPS")
+	serveCmd.Flags().Bool("tls", true, "Enable TLS on listener")
+	serveCmd.Flags().Bool("tls-always-self-sign", false, "Always generate self signed certificate")
+	serveCmd.Flags().Bool("tls-must-staple", false, "Enable TLS must staple")
+	serveCmd.Flags().String("tls-protocols", "tls1.2 tls1.2", "Min and max TLS protocol")
+	serveCmd.Flags().String("tls-cert-file", "", "Path to TLS certificate bundle (concatenation of the server's certificate followed by the CA's certificate chain)")
+	serveCmd.Flags().String("tls-key-file", "", "Path to the server's private key file which matches the certificate bundle")
 
 	return serveCmd
 }
 
 func serve(cmd *cobra.Command, args []string) error {
-	caddy.SetDefaultCaddyfileLoader("default", caddy.LoaderFunc(defaultLoader))
+	caddyArgs := []string{"-type", "http"}
+
+	if agree, _ := cmd.Flags().GetBool("agree"); agree {
+		caddyArgs = append(caddyArgs, "-agree")
+	}
+	if ca, _ := cmd.Flags().GetString("ca"); ca != "" {
+		caddyArgs = append(caddyArgs, "-ca", ca)
+	}
+	if http2, _ := cmd.Flags().GetBool("http2"); http2 {
+		caddyArgs = append(caddyArgs, "-http2")
+	}
+	if quic, _ := cmd.Flags().GetBool("quic"); quic {
+		caddyArgs = append(caddyArgs, "-quic")
+	}
+	if root, _ := cmd.Flags().GetString("root"); root != "" {
+		caddyArgs = append(caddyArgs, "-root", root)
+	}
+	if validate, _ := cmd.Flags().GetBool("validate"); validate {
+		caddyArgs = append(caddyArgs, "-validate")
+	}
+	if revoke, _ := cmd.Flags().GetString("revoke"); revoke != "" {
+		caddyArgs = append(caddyArgs, "-revoke", revoke)
+	}
+
+	email, _ := cmd.Flags().GetString("email")
+	host, _ := cmd.Flags().GetString("host")
+	httpPort, _ := cmd.Flags().GetString("http-port")
+	httpsPort, _ := cmd.Flags().GetString("https-port")
+	tls, _ := cmd.Flags().GetBool("tls")
+	tlsAlwaysSelfSign, _ := cmd.Flags().GetBool("tls-always-self-sign")
+	tlsMustStaple, _ := cmd.Flags().GetBool("tls-must-staple")
+	tlsProtocols, _ := cmd.Flags().GetString("tls-protocols")
+	tlsCertBundle, _ := cmd.Flags().GetString("tls-cert-file")
+	tlsPrivateKey, _ := cmd.Flags().GetString("tls-key-file")
+
+	caddyArgs = append(caddyArgs, "-host", host, "-http-port", httpPort, "-https-port", httpsPort)
+	if tls {
+		caddyArgs = append(caddyArgs, "-port", httpsPort)
+	} else {
+		caddyArgs = append(caddyArgs, "-port", httpPort)
+	}
+	if email != "" {
+		caddyArgs = append(caddyArgs, "-email", email)
+	}
+
+	// Configure underlying caddy.
+	config := &kweb.Config{
+		Host:  host,
+		Email: email,
+
+		TLSEnable:         tls,
+		TLSAlwaysSelfSign: tlsAlwaysSelfSign,
+		TLSCertBundle:     tlsCertBundle,
+		TLSPrivateKey:     tlsPrivateKey,
+		TLSMustStaple:     tlsMustStaple,
+		TLSProtocols:      tlsProtocols,
+	}
+	caddy.SetDefaultCaddyfileLoader("default", defaultLoader(config))
 
 	// Reset args, since caddymain has its own parsing.
-	subArgs := append(os.Args[:1], args...)
+	subArgs := append(os.Args[:1], caddyArgs...)
 	os.Args = subArgs
 
 	caddymain.Run()
@@ -44,14 +118,16 @@ func serve(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
-func defaultLoader(serverType string) (caddy.Input, error) {
-	contents, err := kweb.Caddyfile()
-	if err != nil {
-		return nil, err
-	}
+func defaultLoader(config *kweb.Config) caddy.LoaderFunc {
+	return caddy.LoaderFunc(func(serverType string) (caddy.Input, error) {
+		contents, err := kweb.Caddyfile(config)
+		if err != nil {
+			return nil, err
+		}
 
-	return caddy.CaddyfileInput{
-		Contents:       contents,
-		ServerTypeName: serverType,
-	}, nil
+		return caddy.CaddyfileInput{
+			Contents:       contents,
+			ServerTypeName: serverType,
+		}, nil
+	})
 }
--- config.go
+++ config.go
@@ -5,111 +5,176 @@
 
 package kweb
 
-import ()
+import (
+	"bytes"
+	"fmt"
+)
 
 var caddyfile = []byte(`
-	0.0.0.0
+errors stderr
+log stdout
 
-	tls self_signed
+gzip
 
-	errors stderr
-    log stdout
+header / Server kweb
 
-	gzip
+limits {
+	header 1MB
+	body   50MB
+}
 
-	header / Server kweb
+# Config
+configjson /api/config/v1/kopano/
 
-	limits {
-		header 1MB
-		body   50MB
-	}
+# Konnect
+proxy /upstreams/konnect/ {
+	without /upstreams/konnect/
+	upstream 127.0.0.1:8777
+	policy least_conn
+	health_check /health-check
+	fail_timeout 10s
+	try_duration 30s
+	keepalive 100
+	transparent
+	header_downstream Feature-Policy "midi 'none'"
+	header_downstream X-Frame-Options "sameorigin"
+}
+ratelimit * 100 200 minute {
+	/upstreams/konnect/v1/
+	/signin/v1/identifier/_/
+	whitelist 127.0.0.1/8
+}
+rewrite /.well-known/openid-configuration {
+	to /upstreams/konnect/{path}
+}
+rewrite /konnect/v1/ {
+	to /upstreams/konnect/{path}
+}
+rewrite /signin/v1/ {
+	to /upstreams/konnect/{path}
+}
+redir /signin /signin/v1/identifier
 
-	# Config
-	configjson /api/config/v1/kopano/
+# Kapi
+proxy /upstreams/kapi/ {
+	without /upstreams/kapi/
+	upstream 127.0.0.1:8039
+	policy least_conn
+	health_check /health-check
+	fail_timeout 10s
+	try_duration 30s
+	keepalive 100
+	transparent
+	websocket
+}
+ratelimit * 100 200 minute {
+	/upstreams/kapi/api/
+	whitelist 127.0.0.1/8
+}
+rewrite /api/gc/v1/ {
+	to /upstreams/kapi/{path}
+}
+rewrite /api/pubs/v1/ {
+	to /upstream/kapi/{path}
+}
 
-	# Konnect
-	proxy /upstreams/konnect/ {
-		without /upstreams/konnect/
-		upstream 127.0.0.1:8777
-		policy least_conn
-		health_check /health-check
-		fail_timeout 10s
-		try_duration 30s
-		keepalive 100
-		transparent
-		header_downstream Feature-Policy "midi 'none'"
-		header_downstream X-Frame-Options "sameorigin"
-	}
-	ratelimit * 100 200 minute {
-		/upstreams/konnect/v1/
-		/signin/v1/identifier/_/
-		whitelist 127.0.0.1/8
-	}
-	rewrite /.well-known/openid-configuration {
-		to /upstreams/konnect/{path}
-	}
-	rewrite /konnect/v1/ {
-		to /upstreams/konnect/{path}
-	}
-	rewrite /signin/v1/ {
-		to /upstreams/konnect/{path}
-	}
-	redir /signin /signin/v1/identifier
+# Kwmserver
+proxy /upstreams/kwmserver/ {
+	without /upstreams/kwmserver/
+	upstream 127.0.0.1:8778
+	policy least_conn
+	health_check /health-check
+	fail_timeout 10s
+	try_duration 30s
+	keepalive 100
+	transparent
+	websocket
+}
+ratelimit * 100 200 minute {
+	/upstreams/kwmserver/
+	whitelist 127.0.0.1/8
+}
+rewrite /api/v1/websocket {
+	to /upstreams/kwmserver/{path}
+}
+rewrite /api/v1/rtm.connect {
+	to /upstreams/kwmserver/{path}
+}
+rewrite /api/v1/rtm.turn {
+	to /upstreams/kwmserver/{path}
+}
 
-	# Kapi
-	proxy /upstreams/kapi/ {
-		without /upstreams/kapi/
-		upstream 127.0.0.1:8039
-		policy least_conn
-		health_check /health-check
-		fail_timeout 10s
-		try_duration 30s
-		keepalive 100
-		transparent
-		websocket
-	}
-	ratelimit * 100 200 minute {
-		/upstreams/kapi/api/
-		whitelist 127.0.0.1/8
-	}
-	rewrite /api/gc/v1/ {
-		to /upstreams/kapi/{path}
-	}
-	rewrite /api/pubs/v1/ {
-		to /upstream/kapi/{path}
-	}
-
-	# Kwmserver
-	proxy /upstreams/kwmserver/ {
-		without /upstreams/kwmserver/
-		upstream 127.0.0.1:8778
-		policy least_conn
-		health_check /health-check
-		fail_timeout 10s
-		try_duration 30s
-		keepalive 100
-		transparent
-		websocket
-	}
-	ratelimit * 100 200 minute {
-		/upstreams/kwmserver/
-		whitelist 127.0.0.1/8
-	}
-	rewrite /api/v1/websocket {
-		to /upstreams/kwmserver/{path}
-	}
-	rewrite /api/v1/rtm.connect {
-		to /upstreams/kwmserver/{path}
-	}
-	rewrite /api/v1/rtm.turn {
-		to /upstreams/kwmserver/{path}
-	}
-
-	# Meet
-	staticpwa /meet ./meet-webapp
+# Known Kopano static progressive webapps
+staticpwa /meet ./meet-webapp
+staticpwa /calendar ./calendar-webapp
+staticpwa /mail ./mail-webapp
+staticpwa /contacts ./contacts-webapp
 `)
 
+// Config bundles a bunch of configuration settings.
+type Config struct {
+	Host  string
+	Email string
+
+	TLSEnable         bool
+	TLSAlwaysSelfSign bool
+	TLSCertBundle     string
+	TLSPrivateKey     string
+	TLSKeyType        string
+	TLSProtocols      string
+	TLSMustStaple     bool
+}
+
 // Caddyfile returns a functional caddy file representing our config.
-func Caddyfile() ([]byte, error) {
-	return caddyfile, nil
+func Caddyfile(config *Config) ([]byte, error) {
+	var buf = &bytes.Buffer{}
+
+	// Add host.
+	buf.WriteString(config.Host)
+	buf.WriteString("\n\n")
+
+	// TLS setup
+	if !config.TLSEnable {
+		buf.WriteString("tls off\n\n")
+	} else if (config.Email != "" || (config.TLSCertBundle != "" && config.TLSPrivateKey != "")) && !config.TLSAlwaysSelfSign {
+		buf.WriteString("tls")
+		if config.TLSCertBundle != "" && config.TLSPrivateKey != "" {
+			buf.WriteString(" ")
+			buf.WriteString(config.TLSCertBundle)
+			buf.WriteString(" ")
+			buf.WriteString(config.TLSPrivateKey)
+		}
+		buf.WriteString(" {\n")
+		if config.TLSProtocols != "" {
+			buf.WriteString("protocols ")
+			buf.WriteString(config.TLSProtocols)
+			buf.WriteString("\n")
+		}
+		if config.TLSMustStaple {
+			buf.WriteString("must-staple\n")
+		}
+		if config.TLSKeyType != "" {
+			buf.WriteString("key_type ")
+			buf.WriteString(config.TLSKeyType)
+			buf.WriteString("\n")
+		}
+		buf.WriteString("}\n\n")
+	} else {
+		buf.WriteString("tls self_signed {\n")
+		if config.TLSKeyType != "" {
+			buf.WriteString("key_type ")
+			buf.WriteString(config.TLSKeyType)
+			buf.WriteString("\n")
+		}
+		buf.WriteString("}\n\n")
+	}
+
+	// Add routes.
+	buf.Write(caddyfile)
+
+	// Debug`
+	fmt.Printf("--- cfg start ---\n%s\n-- cfg end --\n", buf.String())
+
+	// Return created config.
+	return buf.Bytes(), nil
 }
